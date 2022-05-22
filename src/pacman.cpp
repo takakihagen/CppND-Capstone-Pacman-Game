63c3ba3916dfa9a6d8138e113249ffd0b0da5e8a
takaki.hagen <takaki.hagen@streamunlimited.com>
2022-05-22 12:18:49 +0200

    GameObject: generate Pacman class
    Signed-off-by: takaki.hagen <takaki.hagen@streamunlimited.com>

---
+++ src/pacman.cpp
@@ -0,0 +1,45 @@
+#include "pacman.h"
+#include "gameObject.h"
+#include <cmath>
+
+Pacman::Pacman() : MovableObject(nullptr, 0, 0, std::mt19937()){}
+Pacman::Pacman(
+    std::shared_ptr<Map> map,
+    std::size_t grid_width, std::size_t grid_height, std::mt19937 engine
+) :   MovableObject(std::move(map), grid_width, grid_height, engine) {
+}
+
+void Pacman::render(SDL_Renderer *sdl_renderer, SDL_Rect block) {
+    SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    block.x = static_cast<int>(x_pos) * block.w;
+    block.y = static_cast<int>(y_pos) * block.h;
+    if (alive) {
+        SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xFF, 0xCC, 0xFF);
+    } else {
+        SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0x00, 0x00, 0xFF);
+    }
+    SDL_RenderFillRect(sdl_renderer, &block);
+}
+
+void Pacman::Update() {
+    switch (direction) {
+    case Direction::kUp:
+      y_pos -= speed;
+      break;
+
+    case Direction::kDown:
+      y_pos += speed;
+      break;
+
+    case Direction::kLeft:
+      x_pos -= speed;
+      break;
+
+    case Direction::kRight:
+      x_pos += speed;
+      break;
+  }
+
+  x_pos = fmod(x_pos + grid_width, grid_width);
+  y_pos = fmod(y_pos + grid_height, grid_height);
+}
---
+++ src/pacman.h
@@ -0,0 +1,17 @@
+#ifndef PACMAN_H
+#define PACMAN_H
+
+#include "movableObject.h"
+#include <random>
+#include <memory>
+
+class Pacman : public MovableObject {
+ public:
+    Pacman();
+    Pacman(std::shared_ptr<Map> map,
+        std::size_t grid_width, std::size_t grid_height, std::mt19937 engine);
+    void render(SDL_Renderer *sdl_renderer, SDL_Rect block) override;
+    void Update() override;
+};
+
+#endif
